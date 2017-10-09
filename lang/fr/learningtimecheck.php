7e7c21d31d1c88869c111f106c38fc2995265aa3
valery <valery.fremaux@gmail.com>
2017-10-09 19:40:47 +0200

    Travis checks

--- lang/fr/learningtimecheck.php
+++ lang/fr/learningtimecheck.php
@@ -59,7 +59,6 @@ $string['checks'] = 'Marques';
 $string['collapseheaders'] = 'Réduire les titres';
 $string['comments'] = 'Commentaires';
 $string['configlastcompiled'] = 'Dernière date de compilation par le cron';
-$string['configlastcompiled_desc'] = 'Changer cette date recompilera les événements non pris en compte.';
 $string['completionboard'] = 'Tableau d\'accomplissements';
 $string['completiongradehelp'] = 'Les notes d\'accomplissement des objectifs sont des scores entiers, pas des pourcentages';
 $string['completionpercent'] = 'Pourcentage d\'éléments non obligatoires marqués attendu';
@@ -69,36 +68,25 @@ $string['completionmandatorygroup'] = 'Nécessite un marquage';
 $string['configallowmodulelinks'] = 'Permettre aux éléments de marquage d\'être liés à des activités de Moodle (peut ralentir l\'affichage des listes)';
 $string['configusestatscoupling'] = 'Couplage avec les Rapports de session';
 $string['configallowoverrideusestats'] = 'permettre la surcharge des Rapports de session';
-$string['configallowoverrideusestats_desc'] = 'Si activé, il est possible de déclarer certains items pour que leur crédit temps remplace la valeur "mesurée" dans les rapports de session de formation.';
 $string['configautoupdateusecron'] = 'Utiliser les tâches programmées pour la mise à jour automatique';
-$string['configautoupdateusecron_desc'] = 'Si activé, la mise à jour automatique des marques est réalisée par les taches planifiées de Moodle, avec un certain retard. Si vous pouvez supporter ce délai, 
-cela évite que les marques soient calculées pendant que vous naviguez dans les interfaces de la liste de temps pédagogique, ce qui peut rallentir celles-ci notablement.';
 $string['configapplyfiltering'] = 'Appliquer le filtrage';
-$string['configapplyfiltering_desc'] = 'Si activé, le fitlrage de jours et heures ouvrées des rapports de temps d\'apprentissage s\'appliquent aux marques générées par la liste. Sinon, le module de temps d\'apprentissage n\'est pas affecté par le filtrage (Sans ipact sur les rapports de session qui continuent à subir le fltrage.).';
 $string['configcsvformat'] = 'Format CSV';
 $string['configcsvencoding'] = 'Encodage';
 $string['configcsvencoding_desc'] = 'Encodage du fichier CSV';
 $string['configcsvfieldseparator'] = 'Séparateur de champ';
-$string['configcsvfieldseparator_desc'] = 'Séparateur de champ CSV';
 $string['configcsvlineseparator'] = 'Fin de ligne';
 $string['configcsvlineseparator_desc'] = 'Fin de ligne CSV';
 $string['configinitialautocapture'] = 'Autocapture initiale';
 $string['configinitialautocapture_desc'] = 'Quel mode de découverte autoamatique des items est activé lors de la création d\'une nouvelle instance de liste';
 $string['configinitiallymandatory'] = 'Initialement obligatoire';
-$string['configinitiallymandatory_desc'] = 'Si marqué, tous les éléments découverts dans les cours seront marqués comme obligatoires. Sinon, tous les éléments créés automatiquement seront initalement marqués comme facultatifs';
 $string['configinitialcredittimeon'] = 'Temps forfaitaires activés initiallement';
-$string['configinitialcredittimeon_desc'] = 'Si marqué, les temps forfaitaires sont disponibles pour toute nouvelle checklist créée';
 $string['configintegrateusestats'] = 'Intégrer les mesures réelles d\'activité';
-$string['configintegrateusestats_desc'] = 'Si activé les d\'activité "au réel" seront intégrées aux rapports';
 $string['configlearningtimecheckautoupdate'] = 'Bénéficier de cette fonctionnalité demande la quelques modifications locales du noyau de Moodle, lisez le document mod/learningtimecheck/README.txt pour plus de détails ou consultez un intégrateur.';
 $string['configlearningtimecheckautoupdateusecron'] = 'Les accomplissements seront automatiquement marqués sur la base d\'événements des traces';
 $string['configmy'] = 'Affichage dans les vues d\'ensemble de cours';
 $string['configshowcompletemymoodle'] = 'Montrer les listes d\'avancement complétées sur les pages personnalisées';
-$string['configshowcompletemymoodle_desc'] = 'Si cette option est activée, alors les listes complétée ne seront pas visible dans les pages personnalisées';
 $string['configshowmymoodle'] = 'Montrer les listes d\'avancement sur les pages personnalisées';
-$string['configshowmymoodle_desc'] = 'Si cette option est activée, alors les listes de progression et leurs barres ne seront pas visible dans les pages personnalisées';
 $string['configcouplecredittomandatoryoption'] = 'Couplage Credit/Obligatoire';
-$string['configcouplecredittomandatoryoption_desc'] = 'Si actif, un item de liste deviendra obligatoire lorsqu\on lui affecte un crédit temps pédagogique non nul';
 $string['confirmdeleteitem'] = 'Etes-vous certain de vouloir définitivement supprimer ces éléments ?';
 $string['coursecalibrationreport'] = 'Rapport de calibrage du cours';
 $string['coursecompletionboard'] = 'Tableau d\'accomplissements du cours';
@@ -167,7 +155,6 @@ $string['linktomodule'] = 'Lier à l\'activité';
 $string['listpreview'] = 'Prévisualisation de la liste';
 $string['myprogress'] = 'Ma progression';
 $string['lockteachermarks'] = 'Verrouiler les marques de l\'enseignant';
-$string['lockteachermarks_help'] = 'Lorsque ce réglage est activé, Les marques validées par les enseignants ne peuvent plus être retirées, sauf par les utilisateurs disposant de la capacité \'mod/learningtimecheck:updatelocked\'.';
 $string['lockteachermarkswarning'] = 'Note: une fois mémorisées, vous ne pouvez plus modifier des marques positives';
 $string['marktypes'] = 'Types de marques';
 $string['mandatory'] = 'obligatoires ';
@@ -191,7 +178,6 @@ $string['percentcomplete'] = 'Eléments obligatoires';
 $string['percentcompleteall'] = 'Tous les éléments';
 $string['pluginadministration'] = 'Admministration de la liste d\'avancement';
 $string['pluginname'] = 'Avancement et temps pédagogiques';
-$string['pluginname_desc'] = 'Ce plugin est basé sur le plugin Checklist et est transformé pour gérer le contrat pédagogique basé sur des affectations de crédit temps aux activités';
 $string['preview'] = 'Prévisualiser';
 $string['progress'] = 'Progression';
 $string['progressbar'] = 'Avancement';
@@ -273,6 +259,7 @@ $string['errornodate'] = 'Erreur : un filtre doit avoir une date';
 $string['errornologop'] = 'Erreur : un filtre doit avoir un opérateur logique';
 $string['itemenable'] = 'Prendre en compte';
 $string['itemdisable'] = 'Ignorer cet élément';
+$string['itemdeclaredtime'] = 'Temps déclaré : {$a} min.';
 
 $string['and'] = 'ET';
 $string['or'] = 'OU';
@@ -293,33 +280,78 @@ $string['firstcoursestarted'] = 'Premier cours démarré ';
 $string['firstcoursecompleted'] = 'Premier cours complété';
 $string['usercohortaddition'] = 'Ajout de l\'utilisateur dans la cohorte ';
 
-$string['modulename_help'] = '
-Ce module aide les étudiants et les enseignants à marquer et suivre les activités accomplies, tout en définissant et comptabilisant des temps pédagogiques associés
-aux activités. Des activités hors ligne peuvent également être comptabilisées, ainsi que des temps déclarés par les étudiants. Les temps validés sont inscrits comme tels
-dans les rapports de Session de Formation (rapport non standard) lié au bloc Mesure d\'Activité. 
+$string['pluginname_desc'] = 'Ce plugin est basé sur le plugin Checklist et est transformé pour gérer le contrat pédagogique basé sur
+des affectations de crédit temps aux activités';
 
-L\'usage de ce module permet de dissocier la notion d\'accomplissement des objectifs pédagogiques
-et règles de poursuite outillées par la notion d\'achèvement du cours, de la notion de compatabilité de temps pédagogique souvent lié à des questions de financement de la formation.
+$string['configinitiallymandatory_desc'] = 'Si marqué, tous les éléments découverts dans les cours seront marqués comme obligatoires.
+Sinon, tous les éléments créés automatiquement seront initalement marqués comme facultatifs';
+
+$string['configinitialcredittimeon_desc'] = 'Si marqué, les temps forfaitaires sont disponibles pour toute nouvelle checklist créée';
+
+$string['configintegrateusestats_desc'] = 'Si activé les d\'activité "au réel" seront intégrées aux rapports';
+
+$string['configshowcompletemymoodle_desc'] = 'Si cette option est activée, alors les listes complétée ne seront pas visible dans les
+pages personnalisées';
+
+$string['configshowmymoodle_desc'] = 'Si cette option est activée, alors les listes de progression et leurs barres ne seront pas visible
+dans les pages personnalisées';
+
+$string['configcouplecredittomandatoryoption_desc'] = 'Si actif, un item de liste deviendra obligatoire lorsqu\'on lui affecte un crédit
+temps pédagogique non nul';
+
+$string['configcsvfieldseparator_desc'] = 'Séparateur de champ CSV';
+
+$string['configallowoverrideusestats_desc'] = 'Si activé, il est possible de déclarer certains items pour que leur crédit temps remplace
+la valeur "mesurée" dans les rapports de session de formation.';
+
+$string['configlastcompiled_desc'] = 'Changer cette date recompilera les événements non pris en compte.';
+
+$string['configautoupdateusecron_desc'] = 'Si activé, la mise à jour automatique des marques est réalisée par les taches planifiées de Moodle,
+avec un certain retard. Si vous pouvez supporter ce délai, cela évite que les marques soient calculées pendant que vous naviguez dans les
+interfaces de la liste de temps pédagogique, ce qui peut rallentir celles-ci notablement.';
+
+$string['configapplyfiltering_desc'] = 'Si activé, le fitlrage de jours et heures ouvrées des rapports de temps d\'apprentissage
+s\'appliquent aux marques générées par la liste. Sinon, le module de temps d\'apprentissage n\'est pas affecté par le filtrage
+(Sans ipact sur les rapports de session qui continuent à subir le fltrage.).';
+
+$string['lockteachermarks_help'] = 'Lorsque ce réglage est activé, Les marques validées par les enseignants ne peuvent plus être retirées,
+sauf par les utilisateurs disposant de la capacité \'mod/learningtimecheck:updatelocked\'.';
+
+$string['modulename_help'] = '
+Ce module aide les étudiants et les enseignants à marquer et suivre les activités accomplies, tout en définissant et comptabilisant des temps
+pédagogiques associés aux activités. Des activités hors ligne peuvent également être comptabilisées, ainsi que des temps déclarés par les étudiants.
+Les temps validés sont inscrits comme tels dans les rapports de Session de Formation (rapport non standard) lié au bloc Mesure d\'Activité.
+
+L\'usage de ce module permet de dissocier la notion d\'accomplissement des objectifs pédagogiques et règles de poursuite outillées par la notion
+d\'achèvement du cours, de la notion de comptabilité de temps pédagogique souvent lié à des questions de financement de la formation.
 ';
 $string['autopopulate_help'] = '
 <p>En activant cette option, vous ajoutez à la liste tous les modules d\'activité selon la portée choisie.</p>
 <p>Vous pouvez choisir d\'intégrer la totalité des modules d\'activité du cours ou seulement ceux de la même section
 que là où est implantée la liste.</p>
-<p>Une fois synchronisée avec le cours, la liste subira tous les changements qui sont faits dans le cours ou la section, quelles que soient vos actions dans le panneau d\'édition des éléments de liste.</p>
+<p>Une fois synchronisée avec le cours, la liste subira tous les changements qui sont faits dans le cours ou la section, quelles que soient
+vos actions dans le panneau d\'édition des éléments de liste.</p>
 <p>Certaines activités pourront être ignorées dans le contrat pédagogique, en cliquant l\'icone "Oeil" à droite de la définition de l\'élément.</p>
-<p>Pour retirer les activités d\'une liste de marquage, vous devez remettre cette option à "Non", puis aller sur le panneau d\'édition des éléments de liste et cliquer sur le bouton "Enlever les activités du cours".</p>
+<p>Pour retirer les activités d\'une liste de marquage, vous devez remettre cette option à "Non", puis aller sur le panneau d\'édition
+des éléments de liste et cliquer sur le bouton "Enlever les activités du cours".</p>
 ';
 
 $string['autoupdate_help'] = '
 <p>Cette fonctionnalité marque automatiquement des activités présentes dans la liste de marquage lorsque certaines actions y sont faites.</p>
-<p>"Compléter" une activité peut dépendre du type d\'activité - la voir pour une ressource, soumettre un fichier pour un devoir, poster un premier message dans un forum, ou rejoindre la première fois un chat, etc.</p>
-<p>Si vous souhaitez des détails sur l\'événement qui "complète" une activité particulière, demandez à votre administrateur de regarder le fichier "mod/learningtimecheck/autoupdatelib.php"</p>
-<p><strong>Note :</strong> La mise à jour des marques automatique dépend du passage de la tâche régulière de mise à jour de Moodle. Il est possible que cette mise à jour puisse prendre plusieurs minutes, selon le réglage de la fréquence de cette tâche. L\'administrateur peut effectuer quelques modifications non standard de Moodle pour rendre cette mise à jour immédiate.</p>
+<p>"Compléter" une activité peut dépendre du type d\'activité - la voir pour une ressource, soumettre un fichier pour un devoir, poster un
+premier message dans un forum, ou rejoindre la première fois un chat, etc.</p>
+<p>Si vous souhaitez des détails sur l\'événement qui "complète" une activité particulière, demandez à votre administrateur de regarder le
+fichier "mod/learningtimecheck/autoupdatelib.php"</p>
+<p><strong>Note :</strong> La mise à jour des marques automatique dépend du passage de la tâche régulière de mise à jour de Moodle. Il
+est possible que cette mise à jour puisse prendre plusieurs minutes, selon le réglage de la fréquence de cette tâche. L\'administrateur peut
+effectuer quelques modifications non standard de Moodle pour rendre cette mise à jour immédiate.</p>
 ';
 
 $string['emailoncomplete_help'] = '
-<p>Quand une liste est compléte (l\'étudiant a coché tous les éléments de la liste ou effectué les activités demandées dans le cas où seul l\'étudiant est concerné, ou l\'enseignant a coché l\'ensemble des éléments, un courriel de notification est envoyé à tous les enseignants du cours.</p>
-<p>Un administrateur peut décider qui reçoit ce courriel en utilisant la capacité "mod:learningtimecheck/emailoncomplete" - par défaut, tous les enseignants et non enseignants non éditeur ont cette capacité.</p>
+<p>Quand une liste est compléte (l\'étudiant a coché tous les éléments de la liste ou effectué les activités demandées dans le cas où seul
+l\'étudiant est concerné, ou l\'enseignant a coché l\'ensemble des éléments, un courriel de notification est envoyé à tous les enseignants du cours.</p>
+<p>Un administrateur peut décider qui reçoit ce courriel en utilisant la capacité "mod:learningtimecheck/emailoncomplete" - par défaut, tous
+les enseignants et non enseignants non éditeur ont cette capacité.</p>
 ';
 
 $string['emailoncompletesubject'] = 'L\'utilisateur {$a->user} a terminé la liste de travaux \'{$a->learningtimecheck}\'';
